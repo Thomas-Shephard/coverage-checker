50407791a1b145c17f01cb28b246293b6d47065d
Thomas Shephard <thomas@thomas-shephard.com>
2024-05-27 16:14:57 +0100

    Add properties to coverage result classes

--- CoverageChecker/Results/FileCoverage.cs
+++ CoverageChecker/Results/FileCoverage.cs
@@ -1,5 +1,7 @@
 namespace CoverageChecker.Results;
 
-public class FileCoverage(LineCoverage[] lines) {
+public class FileCoverage(LineCoverage[] lines, string path, string? packageName = null) {
     public LineCoverage[] Lines { get; } = lines;
+    public string Path { get; } = path;
+    public string? PackageName { get; } = packageName;
 }
