e4d5cbe55dc12d37875b2fa30c0a29e54d0b0d94
Thomas Shephard <thomas@thomas-shephard.com>
2024-05-27 21:03:40 +0100

    Add unit tests for methods to get file and line coverage results

--- CoverageChecker.Tests/ResultTests/CoverageTests.cs
+++ CoverageChecker.Tests/ResultTests/CoverageTests.cs
@@ -29,4 +29,52 @@ public class CoverageTests {
 
         Assert.That(coverage.Files, Is.EqualTo(files));
     }
+
+    [Test]
+    public void Coverage_TryGetFile_FileExists_ReturnsTrue() {
+        FileCoverage[] files = [
+            new FileCoverage([
+                new LineCoverage(1, true, 1, 0),
+                new LineCoverage(2, false, 8, 1),
+                new LineCoverage(3, true, 2, 1)
+            ], "coverage-file-1"),
+            new FileCoverage([
+                new LineCoverage(1, true, 1, 0),
+                new LineCoverage(2, true, 3, 2),
+                new LineCoverage(3, false, 4, 3)
+            ], "coverage-file-2")
+        ];
+        Coverage coverage = new(files);
+
+        bool result = coverage.TryGetFile("coverage-file-1", out FileCoverage? file);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.True);
+            Assert.That(file, Is.EqualTo(files[0]));
+        });
+    }
+
+    [Test]
+    public void Coverage_TryGetFile_FileDoesNotExist_ReturnsFalse() {
+        FileCoverage[] files = [
+            new FileCoverage([
+                new LineCoverage(1, true, 1, 0),
+                new LineCoverage(2, false, 8, 1),
+                new LineCoverage(3, true, 2, 1)
+            ], "coverage-file-1"),
+            new FileCoverage([
+                new LineCoverage(1, true, 1, 0),
+                new LineCoverage(2, true, 3, 2),
+                new LineCoverage(3, false, 4, 3)
+            ], "coverage-file-2")
+        ];
+        Coverage coverage = new(files);
+
+        bool result = coverage.TryGetFile("coverage-file-3", out FileCoverage? file);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.False);
+            Assert.That(file, Is.Null);
+        });
+    }
 }
--- CoverageChecker.Tests/ResultTests/FileCoverageTests.cs
+++ CoverageChecker.Tests/ResultTests/FileCoverageTests.cs
@@ -26,4 +26,40 @@ public class FileCoverageTests {
             Assert.That(fileCoverage.PackageName, Is.EqualTo("package-name"));
         });
     }
+
+    [Test]
+    public void FileCoverage_TryGetLine_LineExists_ReturnsTrue() {
+        LineCoverage[] lines = [
+            new LineCoverage(1, true, 1, 0),
+            new LineCoverage(2, false, 6, 2),
+            new LineCoverage(3, true, 4, 3)
+        ];
+        FileCoverage fileCoverage = new(lines, "coverage-file");
+
+        bool result = fileCoverage.TryGetLine(1, out LineCoverage? line);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.True);
+            Assert.That(line, Is.EqualTo(lines[0]));
+        });
+    }
+
+    [Test]
+    public void FileCoverage_TryGetLine_LineDoesNotExist_ReturnsFalse() {
+        LineCoverage[] lines = [
+            new LineCoverage(1, true, 1, 0),
+            new LineCoverage(2, false, 6, 2),
+            new LineCoverage(3, true, 4, 3)
+        ];
+        FileCoverage fileCoverage = new(lines, "coverage-file");
+
+        bool result = fileCoverage.TryGetLine(4, out LineCoverage? line);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.False);
+            Assert.That(line, Is.Null);
+            Assert.That(fileCoverage.Path, Is.EqualTo("coverage-file"));
+            Assert.That(fileCoverage.PackageName, Is.Null);
+        });
+    }
 }
