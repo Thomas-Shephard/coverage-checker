36274baf292f60174326e002f1ceee8da29b605c
Thomas Shephard <thomas@thomas-shephard.com>
2024-05-27 16:48:39 +0100

    Add methods to get file and line coverage results

--- CoverageChecker/Results/Coverage.cs
+++ CoverageChecker/Results/Coverage.cs
@@ -2,4 +2,9 @@ namespace CoverageChecker.Results;
 
 public class Coverage(FileCoverage[] files) {
     public FileCoverage[] Files { get; } = files;
+
+    public bool TryGetFile(string path, out FileCoverage? file) {
+        file = Files.FirstOrDefault(file => file.Path == path);
+        return file is not null;
+    }
 }
--- CoverageChecker/Results/FileCoverage.cs
+++ CoverageChecker/Results/FileCoverage.cs
@@ -4,4 +4,9 @@ public class FileCoverage(LineCoverage[] lines, string path, string? packageName
     public LineCoverage[] Lines { get; } = lines;
     public string Path { get; } = path;
     public string? PackageName { get; } = packageName;
+
+    public bool TryGetLine(int lineNumber, out LineCoverage? line) {
+        line = Lines.FirstOrDefault(line => line.LineNumber == lineNumber);
+        return line is not null;
+    }
 }
