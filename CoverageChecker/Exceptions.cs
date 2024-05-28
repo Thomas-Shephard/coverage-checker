d34f60213994e044061edf93855b762546b9c313
Thomas Shephard <thomas@thomas-shephard.com>
2024-05-28 21:02:23 +0100

    Add exception for invalid coverage files

--- CoverageChecker/Exceptions.cs
+++ CoverageChecker/Exceptions.cs
@@ -3,3 +3,5 @@ namespace CoverageChecker;
 public class CoverageException(string message) : Exception(message);
 
 public class CoverageCalculationException(string message) : CoverageException(message);
+
+public class CoverageParseException(string message) : CoverageException(message);
