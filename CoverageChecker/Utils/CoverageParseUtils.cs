79ecab43d1da4e641ca48da33a492a24c074bac0
Thomas Shephard <thomas@thomas-shephard.com>
2024-05-29 20:32:49 +0100

    Add utility methods to parse coverage files (#14)
    * Add exception for invalid coverage files
    
    * Add utility methods to parse coverage files
    
    * Add unit tests for utility methods to parse coverage files

---
+++ CoverageChecker/Utils/CoverageParseUtils.cs
@@ -0,0 +1,36 @@
+using System.Xml.Linq;
+
+namespace CoverageChecker.Utils;
+
+internal static class CoverageParseUtils {
+    internal static XElement GetRequiredElement(this XContainer container, XName name) {
+        return container.Element(name) ?? throw new CoverageParseException($"Element '{name}' not found");
+    }
+
+    internal static XAttribute GetRequiredAttribute(this XElement element, XName name) {
+        return element.Attribute(name) ?? throw new CoverageParseException($"Attribute '{name}' not found on element '{element.Name}'");
+    }
+
+    internal static T? ParseOptionalAttribute<T>(this XElement element, XName name, IFormatProvider? provider = null) where T : struct, IParsable<T> {
+        string? attributeValue = element.Attribute(name)?.Value;
+
+        if (attributeValue is null)
+            return null;
+
+        // If the attribute is found, try to parse the value into the specified type
+        if (!T.TryParse(attributeValue, provider, out T value))
+            throw new CoverageParseException($"Attribute '{name}' on element '{element.Name}' is not a valid {typeof(T).Name}");
+
+        return value;
+    }
+
+    internal static T ParseRequiredAttribute<T>(this XElement element, XName name, IFormatProvider? provider = null) where T : IParsable<T> {
+        string attributeValue = element.GetRequiredAttribute(name).Value;
+
+        // Try to parse the attribute value into the specified type
+        if (!T.TryParse(attributeValue, provider, out T? value))
+            throw new CoverageParseException($"Attribute '{name}' on element '{element.Name}' is not a valid {typeof(T).Name}");
+
+        return value;
+    }
+}
