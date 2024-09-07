923acfdd3bbd50503e93ad9c86d10393821bae5c
Thomas Shephard <thomas@thomas-shephard.com>
2024-09-07 20:51:28 +0100

    Update testing of utility methods that parse coverage files

---
+++ tests/CoverageChecker.UnitTests/UtilTests/CoverageFileParseUtilTests/TryGetAttributeTests.cs
@@ -0,0 +1,85 @@
+using System.Xml;
+using CoverageChecker.Utils;
+
+namespace CoverageChecker.UnitTests.UtilTests.CoverageFileParseUtilTests;
+
+public class TryGetAttributeTests {
+    [Test]
+    public void CoverageFileParseUtils_TryGetAttribute_StringAttributeFound_ReturnsValue() {
+        const string attributeValue = "value";
+        const string xml = $"""<{XmlReaderTestUtils.ElementName} {XmlReaderTestUtils.AttributeName}="{attributeValue}"/>""";
+
+        XmlReader reader = XmlReaderTestUtils.CreateXmlReader(xml);
+
+        reader.MoveTo(XmlReaderTestUtils.ElementName, XmlNodeType.Element);
+
+        bool attributeFound = reader.TryGetAttribute(XmlReaderTestUtils.AttributeName, out string? attribute);
+
+        Assert.Multiple(() => {
+            Assert.That(attributeFound, Is.True);
+            Assert.That(attribute, Is.EqualTo(attributeValue));
+        });
+    }
+
+    [Test]
+    public void CoverageFileParseUtils_TryGetAttribute_StringAttributeNotFound_ReturnsDefault() {
+        const string xml = $"<{XmlReaderTestUtils.ElementName}/>";
+
+        XmlReader reader = XmlReaderTestUtils.CreateXmlReader(xml);
+
+        reader.MoveTo(XmlReaderTestUtils.ElementName, XmlNodeType.Element);
+
+        bool attributeFound = reader.TryGetAttribute(XmlReaderTestUtils.AttributeName, out string? attribute);
+
+        Assert.Multiple(() => {
+            Assert.That(attributeFound, Is.False);
+            Assert.That(attribute, Is.Default);
+        });
+    }
+
+    [Test]
+    public void CoverageFileParseUtils_TryGetAttribute_IntAttributeFound_ReturnsValue() {
+        const string attributeValue = "42";
+        const string xml = $"""<{XmlReaderTestUtils.ElementName} {XmlReaderTestUtils.AttributeName}="{attributeValue}"/>""";
+
+        XmlReader reader = XmlReaderTestUtils.CreateXmlReader(xml);
+
+        reader.MoveTo(XmlReaderTestUtils.ElementName, XmlNodeType.Element);
+
+        bool attributeFound = reader.TryGetAttribute(XmlReaderTestUtils.AttributeName, out int attribute);
+
+        Assert.Multiple(() => {
+            Assert.That(attributeFound, Is.True);
+            Assert.That(attribute, Is.EqualTo(int.Parse(attributeValue)));
+        });
+    }
+
+    [Test]
+    public void CoverageFileParseUtils_TryGetAttribute_IntAttributeNotFound_ReturnsDefault() {
+        const string xml = $"<{XmlReaderTestUtils.ElementName}/>";
+
+        XmlReader reader = XmlReaderTestUtils.CreateXmlReader(xml);
+
+        reader.MoveTo(XmlReaderTestUtils.ElementName, XmlNodeType.Element);
+
+        bool attributeFound = reader.TryGetAttribute(XmlReaderTestUtils.AttributeName, out int attribute);
+
+        Assert.Multiple(() => {
+            Assert.That(attributeFound, Is.False);
+            Assert.That(attribute, Is.Default);
+        });
+    }
+
+    [Test]
+    public void CoverageFileParseUtils_TryGetAttribute_IntAttributeFoundButInvalid_ThrowsException() {
+        const string attributeValue = "not-a-number";
+        const string xml = $"""<{XmlReaderTestUtils.ElementName} {XmlReaderTestUtils.AttributeName}="{attributeValue}"/>""";
+
+        XmlReader reader = XmlReaderTestUtils.CreateXmlReader(xml);
+
+        reader.MoveTo(XmlReaderTestUtils.ElementName, XmlNodeType.Element);
+
+        Exception e = Assert.Throws<CoverageParseException>(() => reader.TryGetAttribute<int>(XmlReaderTestUtils.AttributeName, out _));
+        Assert.That(e.Message, Is.EqualTo($"Failed to parse attribute '{XmlReaderTestUtils.AttributeName}' on node '{XmlReaderTestUtils.ElementName}'"));
+    }
+}
---
+++ tests/CoverageChecker.UnitTests/UtilTests/CoverageFileParseUtilTests/XmlReaderTestUtils.cs
@@ -0,0 +1,39 @@
+using System.Xml;
+using CoverageChecker.Parsers;
+
+namespace CoverageChecker.UnitTests.UtilTests.CoverageFileParseUtilTests;
+
+internal static class XmlReaderTestUtils {
+    internal const string AttributeName = "attribute";
+    internal const string ElementName = "element";
+    internal const string ChildElementName = "child";
+    internal const string GrandchildElementName = "grandchild";
+    internal const string GreatGrandchildElementName = "great-grandchild";
+
+    internal const string UnknownElementName = "unknown";
+
+    internal static XmlReader CreateXmlReader(string xml) {
+        return XmlReader.Create(new StringReader(xml), BaseParser.XmlReaderSettings);
+    }
+
+    internal static IXmlLineInfo GetXmlLineInfo(XmlReader reader) {
+        if (reader is IXmlLineInfo lineInfo)
+            return lineInfo;
+
+        throw new InvalidCastException("This reader does not support line info");
+    }
+
+    internal static void MoveTo(this XmlReader reader, string elementName, XmlNodeType nodeType) {
+        while (reader.Name != elementName || reader.NodeType != nodeType) {
+            if (!reader.Read())
+                throw new CoverageParseException($"Could not move to element '{elementName}'");
+        }
+    }
+
+    internal static void CheckPosition(this IXmlLineInfo lineInfo, int lineNumber, int linePosition) {
+        Assert.Multiple(() => {
+            Assert.That(lineInfo.LineNumber, Is.EqualTo(lineNumber));
+            Assert.That(lineInfo.LinePosition, Is.EqualTo(linePosition));
+        });
+    }
+}
