1b9772726533035ab5690b25574ea52ac0a1da5d
Thomas Shephard <thomas@thomas-shephard.com>
2024-10-20 21:43:43 +0100

    fix: command line default glob patterns matching xml files

--- src/CoverageChecker.CommandLine/CommandLineOptions.cs
+++ src/CoverageChecker.CommandLine/CommandLineOptions.cs
@@ -9,6 +9,6 @@ public class CommandLineOptions {
     [Option('d', "directory", Required = false, HelpText = "Directory where coverage files are located. Default: Current directory")]
     public string Directory { get; init; } = Environment.CurrentDirectory;
 
-    [Option('g', "glob-patterns", Required = false, HelpText = "Glob patterns of coverage file locations. Default: *.xml")]
+    [Option('g', "glob-patterns", Required = false, HelpText = "Glob patterns of coverage file locations. Default: *.xml", Default = new[] { "*.xml" })]
     public IEnumerable<string> GlobPatterns { get; init; } = ["*.xml"];
 }
