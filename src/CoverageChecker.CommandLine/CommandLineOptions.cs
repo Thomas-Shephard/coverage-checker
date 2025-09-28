fb5cfc6aff8f956fd5702e8d0e8d702226768696
Thomas Shephard <thomas@thomas-shephard.com>
2025-09-28 12:47:49 +0100

    feat: add coverage thresholds to cli

--- src/CoverageChecker.CommandLine/CommandLineOptions.cs
+++ src/CoverageChecker.CommandLine/CommandLineOptions.cs
@@ -12,4 +12,37 @@ public class CommandLineOptions
 
     [Option('g', "glob-patterns", Required = false, HelpText = "Glob patterns of coverage file locations. Default: *.xml", Default = new[] { "*.xml" })]
     public IEnumerable<string> GlobPatterns { get; init; } = ["*.xml"];
+
+    private readonly double _lineThreshold = 0.8;
+    [Option('l', "line-threshold", Required = false, HelpText = "Line coverage threshold (percentage). Default: 80")]
+    public double LineThreshold
+    {
+        get => _lineThreshold;
+        init
+        {
+            if (value is < 0 or > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(LineThreshold), "Line threshold must be between 0 and 100");
+            }
+
+            _lineThreshold = value / 100;
+        }
+    }
+
+    private readonly double _branchThreshold = 0.8;
+
+    [Option('b', "branch-threshold", Required = false, HelpText = "Branch coverage threshold (percentage). Default: 80")]
+    public double BranchThreshold
+    {
+        get => _branchThreshold;
+        init
+        {
+            if (value is < 0 or > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(BranchThreshold), "Branch threshold must be between 0 and 100");
+            }
+
+            _branchThreshold = value / 100;
+        }
+    }
 }
