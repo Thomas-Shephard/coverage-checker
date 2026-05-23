d2408c32bfe7872878acd3a23a9441ed5548b467
Thomas Shephard <thomas@thomas-shephard.com>
2026-05-23 20:38:24 +0100

    fix: dogfood generated OpenCover coverage

--- tests/CoverageChecker.Tests.Unit/ParserTests/OpenCoverParserTests.cs
+++ tests/CoverageChecker.Tests.Unit/ParserTests/OpenCoverParserTests.cs
@@ -49,6 +49,53 @@ public class OpenCoverParserTests
         }
     }
 
+    [Test]
+    public void ParseCoverageWithHigherKnownBranchCountThenLowerBranchCountUsesKnownBranchCount()
+    {
+        string directory = Path.Combine(TestContext.CurrentContext.WorkDirectory, Guid.NewGuid().ToString());
+        Directory.CreateDirectory(directory);
+
+        string coverageWithFourBranches = Path.Combine(directory, "with-four-branches.xml");
+        string coverageWithTwoBranches = Path.Combine(directory, "with-two-branches.xml");
+
+        File.WriteAllText(coverageWithFourBranches, CreateCoverageFile("""
+            <SequencePoint vc="1" uspid="1" ordinal="0" offset="0" sl="60" fileid="1" />
+            """, """
+            <BranchPoint vc="1" uspid="2" ordinal="0" offset="0" sl="60" fileid="1" path="0" />
+            <BranchPoint vc="1" uspid="3" ordinal="1" offset="0" sl="60" fileid="1" path="1" />
+            <BranchPoint vc="1" uspid="4" ordinal="2" offset="0" sl="60" fileid="1" path="2" />
+            <BranchPoint vc="1" uspid="5" ordinal="3" offset="0" sl="60" fileid="1" path="3" />
+            """));
+
+        File.WriteAllText(coverageWithTwoBranches, CreateCoverageFile("""
+            <SequencePoint vc="1" uspid="1" ordinal="0" offset="0" sl="60" fileid="1" />
+            """, """
+            <BranchPoint vc="1" uspid="2" ordinal="0" offset="0" sl="60" fileid="1" path="0" />
+            <BranchPoint vc="1" uspid="3" ordinal="1" offset="0" sl="60" fileid="1" path="1" />
+            """));
+
+        try
+        {
+            Coverage coverage = new();
+            OpenCoverParser parser = new(coverage, NullLogger<OpenCoverParser>.Instance, new CoverageMergeService());
+
+            parser.ParseCoverage(coverageWithFourBranches);
+            parser.ParseCoverage(coverageWithTwoBranches);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(coverage.Files, Has.Count.EqualTo(1));
+                Assert.That(coverage.Files[0].Lines, Has.Count.EqualTo(1));
+                Assert.That(coverage.Files[0].Lines[0].Branches, Is.EqualTo(4));
+                Assert.That(coverage.Files[0].Lines[0].CoveredBranches, Is.EqualTo(4));
+            }
+        }
+        finally
+        {
+            Directory.Delete(directory, true);
+        }
+    }
+
     private static string CreateCoverageFile(string sequencePoints, string branchPoints)
     {
         return $$"""
